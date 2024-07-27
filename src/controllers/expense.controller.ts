ea39fec7b402075ff01d9e535fd6549d5012b9e9
Gideon Chidi <gideonchidi471@gmail.com>
2024-07-27 23:35:09 +0100

    chore: done with update expense endpoint

--- src/controllers/expense.controller.ts
+++ src/controllers/expense.controller.ts
@@ -28,5 +28,47 @@ async function addExpense(req: Request, res: Response, next: NextFunction) {
     next(error);
   }
 }
+async function updateEvent(req: Request, res: Response) {
+  //@ts-ignore
+  const { userId } = req.user;
 
-export default addExpense;
+  const { expenseId } = req.params;
+  const { body } = req;
+  const {
+    success,
+    error,
+    data: expenseDetails,
+  } = expenseSchema.safeParse(body);
+  if (!success) {
+    return res.status(400).json({
+      error: convertZodEror(error),
+    });
+  }
+  console.log(expenseId, req.query, req.params);
+  const updatedExpense = await ExpenseModel.findByIdAndUpdate(
+    expenseId,
+    {
+      name: expenseDetails.name,
+      category: expenseDetails.category,
+      amount: expenseDetails.amount,
+      userId,
+      updatedAt: Date.now(),
+    },
+    {
+      projection: {
+        _id: 0,
+        __v: 0,
+      },
+    }
+  );
+  if (!updatedExpense) {
+    return res.status(404).json({
+      message: "Expense not found",
+    });
+  }
+
+  return res.status(200).json({
+    message: "Expense updated",
+  });
+}
+export { addExpense, updateEvent };
--- src/routes/expense.route.ts
+++ src/routes/expense.route.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import isAuthenticated from "../middlewares/isAuthenticated";
-import addExpense from "../controllers/expense.controller";
+import { addExpense, updateEvent } from "../controllers/expense.controller";
 
 const expenseRouter = Router();
 expenseRouter.use(isAuthenticated);
 expenseRouter.post("/", addExpense);
+expenseRouter.put("/:expenseId", updateEvent);
 
 export default expenseRouter;
